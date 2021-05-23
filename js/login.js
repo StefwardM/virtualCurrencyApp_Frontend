fc3fec171250a87b2879d3042e72bae171ef1caa
StefwardM <stefanmladjenovic@hotmail.com>
2021-05-23 16:22:52 +0200

    First version, added signup, login and app.html

---
+++ js/login.js
@@ -0,0 +1,30 @@
+/*const btnSignup = */document.querySelector(".login button").addEventListener("click", () => {
+    let username = document.querySelector('#email').value;
+    let password = document.querySelector('#password').value;
+
+    fetch('http://localhost:3000/users/login', {
+        method: "post",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "username":username,
+            "password": password
+        })
+    }).then(response => {
+        return response.json();
+    }).then(json => {
+        if(json.status === "success") {
+
+            let token = json.data.token;
+            localStorage.setItem("token", token);
+            window.location.href = "app.html";
+        }
+        else{
+            let feedback = document.querySelector(".alert");
+            feedback.textContent = "Login failed compadre!";
+            feedback.classList.remove('hidden');
+        }
+
+    })
+});
