62c799700725751e03bcb1d691d30303bc0d3481
Gunnar Van Remoortere <gunnarvanremoortere@hotmail.com>
2021-05-28 16:04:56 +0200

    small changes, forgot to switch to my branch

--- js/transfer.js
+++ js/transfer.js
@@ -23,6 +23,26 @@ document.querySelector("#confirmBtn").addEventListener("click", () => {
         console.log(json);
         let transferId = json.data.transfer['_id'];
         let amount = json.data.transfer
+        fetch('http://localhost:3000/api/v1/transfers'+transferId, {
+            method: "put",
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + localStorage.getItem('token')
+            },
+            body: JSON.stringify({
+                "recipient": sendTo,
+                "reason": reason,
+                "message": message,
+                "amount": amount
+            })
+        }).then(response => {
+            return response.json();
+        }).then(json => {
+            console.log(json);
+            let transferId = json.data.transfer['_id'];
+            let amount = json.data.transfer
+
+        })
         // window.location.href = "app.html";
     })
 });
