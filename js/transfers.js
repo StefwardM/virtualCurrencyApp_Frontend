1382edc188722ddb8980fc8942338f20649e70b6
Gunnar Van Remoortere <gunnarvanremoortere@hotmail.com>
2021-05-30 23:00:24 +0200

    error messages on transfers and history page

--- js/transfers.js
+++ js/transfers.js
@@ -9,28 +9,35 @@ fetch(base_url + '/api/v1/transfers', {
 }).then(response => {
     return response.json();
 }).then(json => {
-    console.log(json);
-    let user = json.ppname;
 
-    for (let i = json.data.transfers.length - 1; i >= 0 ; i--){
-        if(json.data.transfers[i]['sender'] === user) {
-            let outgoing =
-                `<li class="flex flex-row justify-around transaction mb-4">
+    if(json.status === "success") {
+        let user = json.ppname;
+
+        for (let i = json.data.transfers.length - 1; i >= 0 ; i--){
+            if(json.data.transfers[i]['sender'] === user) {
+                let outgoing =
+                    `<li class="flex flex-row justify-around transaction mb-4">
                 <p class="p--normal transaction__p transaction--outgoing">To ${json.data.transfers[i]['recipient']}</p>
                 <p class="p--normal transaction__p transaction--outgoing">${json.data.transfers[i]['amount']} PP</p>
                 </li>`
-            document.querySelector("#transfers").insertAdjacentHTML('beforeend', outgoing);
-        }
+                document.querySelector("#transfers").insertAdjacentHTML('beforeend', outgoing);
+            }
 
-        if(json.data.transfers[i]['recipient'] === user) {
-            let incoming =
-                `<li class="flex flex-row justify-around transaction mb-4">
+            if(json.data.transfers[i]['recipient'] === user) {
+                let incoming =
+                    `<li class="flex flex-row justify-around transaction mb-4">
                 <p class="p--normal transaction__p transaction--incoming">From ${json.data.transfers[i]['sender']}</p>
                 <p class="p--normal transaction__p transaction--incoming">${json.data.transfers[i]['amount']} PP</p>
                 </li>`
-            document.querySelector("#transfers").insertAdjacentHTML('beforeend', incoming);
+                document.querySelector("#transfers").insertAdjacentHTML('beforeend', incoming);
+            }
         }
     }
-}).catch(err => {
-    console.log(err);
+    else{
+        let errorDiv = document.querySelector(".error");
+        let errorText = document.querySelector(".error p");
+        errorText.innerHTML = json.message;
+        errorDiv.classList.remove('hidden');
+    }
+
 })
